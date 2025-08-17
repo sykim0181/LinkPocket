ca56a538a3dd9ba58c96485d8630c89a0376dd9f
sykim0181 <soyeon364@naver.com>
2025-08-17 23:20:14 +0900

    feat: add, view 페이지 생성

---
+++ src/app/add/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Add Page</h1>
+      <p>This is the add page.</p>
+    </div>
+  );
+};
+export default Page;
---
+++ src/app/view/[id]/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>View Page</h1>
+      <p>This is the view page.</p>
+    </div>
+  );
+};
+export default Page;
