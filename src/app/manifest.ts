b169680d64b3bf66c23bd2567519289b6a1ab250
sykim0181 <soyeon364@naver.com>
2025-08-17 18:03:48 +0900

    chore: icon 변경

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "/icon-32.png",
+        src: "/icon-192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icon-96.png",
+        src: "/icon-512.png",
         sizes: "512x512",
         type: "image/png",
       },
