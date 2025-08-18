509b82f34d9f79f6aeae78b1c544b0e2df0d2c4a
sykim0181 <soyeon364@naver.com>
2025-08-19 01:35:03 +0900

    feat: 헤더 클릭 시 home으로 이동

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,11 @@
+import Link from "next/link";
+
 const Header = () => {
-  return <header className="text-3xl text-center p-4">LinkPocket</header>;
+  return (
+    <header className="text-3xl text-center p-4">
+      <Link href="/">LinkPocket</Link>
+    </header>
+  );
 };
 
 export default Header;
